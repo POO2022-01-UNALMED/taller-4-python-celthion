52b466018237bfca1c6983d35fb13f0c1728e831
David Cardona Duque <davcardona@unal.edu.co>
2022-04-03 17:38:41 -0500

    :3

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -6,32 +6,43 @@ from classroom.asignatura import Asignatura
 
 class Grupo:
 
-    grado = None
+    grado = "Grado 12"
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes=None):
+    def __init__(self, grupo="grupo ordinario",asignaturas=[], estudiantes=[]):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
 
-    def listadoAsignaturas(self, kwargs):
+    def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista=[]):
-        if(lista is None):
+    def agregarAlumno(self, alumno, lista=None):
+        if (lista is None):
+            lista=[]
             lista.append(alumno)
-            self.listadoAlumnos = self.listadoAlumnos + lista
+            self.listadoAlumnos = lista + self.listadoAlumnos
         else:
-            self.listadoAlumnos = [alumno]
+            self.listadoAlumnos =  lista + [alumno]
+
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 10"):
         cls.grado = nombre
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 6"):
-        cls.grado = nombre
+
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 4"):
         cls.grado = nombre
+
+    @classmethod
+    def asignarNombre(cls, nombre="Grado 6"):
+        cls.grado = nombre
+
+    def __str__(self):
+        if self._grupo=="grupo ordinario":
+            cadena="Grupo de estudiantes: grupo predeterminado"
+            return cadena
+        else:
+            return ""
